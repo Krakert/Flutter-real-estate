8279bd2719dec29965d4482d21c0d18b20ca7586
Stefan de Kraker <stefandekraker1999@gmail.com>
2023-09-27 14:25:32 +0200

    Setup of Colours and Typography

---
+++ lib/ui/theme/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const dttRed = Color(0xFFE65541);
+  static const white = Color(0xFFFFFFFF);
+  static const lightGray = Color(0xFFF7F7F7);
+  static const darkGray = Color(0xFFEBEBEB);
+  static const strong = Color(0xCC000000);
+  static const medium = Color(0x66000000);
+  static const light = Color(0x33000000);
+}
---
+++ lib/ui/theme/type.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_real_estate/ui/theme/colors.dart';
+
+class AppTypography {
+  static const title01 = TextStyle(
+    fontFamily: "GothamSSm",
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    color: AppColors.strong,
+  );
+
+  static const title02 = TextStyle(
+    fontFamily: "GothamSSm",
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: AppColors.strong,
+  );
+
+  static const title03 = TextStyle(
+    fontFamily: "GothamSSm",
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: AppColors.strong,
+  );
+
+  static const body = TextStyle(
+    fontFamily: "GothamSSm",
+    fontSize: 12,
+    fontWeight: FontWeight.w300,
+    color: AppColors.strong,
+  );
+
+  static const input = TextStyle(
+    fontFamily: "GothamSSm",
+    fontSize: 12,
+    fontWeight: FontWeight.w300,
+  );
+
+  static const hint = TextStyle(
+    fontFamily: "GothamSSm",
+    fontSize: 12,
+    fontWeight: FontWeight.w300,
+    color: AppColors.strong,
+  );
+
+  static const subtitle = TextStyle(
+    fontFamily: "GothamSSm",
+    fontSize: 10,
+    fontWeight: FontWeight.w300,
+    color: AppColors.strong,
+  );
+
+  static const detail = TextStyle(
+    fontFamily: "GothamSSm",
+    fontSize: 12,
+    fontWeight: FontWeight.w300,
+    color: AppColors.medium,
+  );
+}
