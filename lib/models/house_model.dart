e6ca51ebf206579bdd4308ea2d35782a5986de31
Stefan de Kraker <stefandekraker1999@gmail.com>
2023-09-27 14:26:48 +0200

    Init house_model.dart

---
+++ lib/models/house_model.dart
@@ -0,0 +1,46 @@
+class HouseData {
+  final int id;
+  final int price;
+  final String image;
+  final String zip;
+  final int bathrooms;
+  final int bedrooms;
+  final int size;
+  final String city;
+  final String description;
+  final int latitude;
+  final int longitude;
+  final String createdDate;
+
+  HouseData({
+    required this.id,
+    required this.price,
+    required this.image,
+    required this.zip,
+    required this.bathrooms,
+    required this.bedrooms,
+    required this.size,
+    required this.city,
+    required this.description,
+    required this.latitude,
+    required this.longitude,
+    required this.createdDate,
+  });
+
+  factory HouseData.fromJson(Map<String, dynamic> json) {
+    return HouseData(id: json['id'],
+        price: json['price'],
+        image: json['image'],
+        zip: json['zip'],
+        bathrooms: json['bathrooms'],
+        bedrooms: json['bedrooms'],
+        size: json['size'],
+        city: json['city'],
+        description: json['description'],
+        latitude: json['latitude'],
+        longitude: json['longitude'],
+        createdDate: json['createdDate'],
+    );
+  }
+
+}
