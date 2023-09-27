62cc8ef086cde53436f929bbb5dea7d96bd05d41
Stefan de Kraker <stefandekraker1999@gmail.com>
2023-09-27 16:30:18 +0200

    Setup API exceptions

---
+++ lib/data/api_exception.dart
@@ -0,0 +1,20 @@
+sealed class APIException implements Exception {
+  APIException(this.message);
+  final String message;
+}
+
+class InvalidApiKeyException extends APIException {
+  InvalidApiKeyException() : super('Invalid API key');
+}
+
+class NoInternetConnectionException extends APIException {
+  NoInternetConnectionException() : super('No Internet connection');
+}
+
+class NotFoundException extends APIException {
+  NotFoundException() : super('City not found');
+}
+
+class UnknownException extends APIException {
+  UnknownException() : super('Some error occurred');
+}
