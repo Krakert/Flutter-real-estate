89ed4f8a4d5c359a630430629df614bd6bef12f4
Stefan de Kraker <stefandekraker1999@gmail.com>
2023-09-29 20:24:17 +0200

    Added extra URls

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -2,4 +2,6 @@ class Constants {
   static const dttHomeUrl = 'https://d-tt.nl';
   static const baseAPIUrl = 'https://intern.d-tt.nl';
   static const houseAPIUrl = '$baseAPIUrl/api/house';
+  static const githubUrl = 'https://github.com/Krakert';
+  static const linkedinUrl = 'https://www.linkedin.com/in/stefan-de-kraker/';
 }
