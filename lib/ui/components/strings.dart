b9f08b99586876f7e6bb1eb76d9e3653a615d412
Stefan de Kraker <stefandekraker1999@gmail.com>
2023-09-30 11:53:14 +0200

    Moved strings to separate file

---
+++ lib/ui/components/strings.dart
@@ -0,0 +1,20 @@
+class Strings {
+  Strings._();
+
+  static const appBarTitles = ['DTT REAL ESTATE', 'ABOUT'];
+
+  static const searchBarHint = 'Search for a home';
+
+  static const aboutText =
+      'Hello, I am Stefan de Kraker, a recently graduated Technical Informatics student with a great passion for hardware and software.'
+      '\n\nDuring my studies I took a minor in mobile application development. Since then, my love and passion for mobile app development has only grown.'
+      '\n\nAlthough developing mobile applications fell outside my regular course of study, I was determined to master new techniques and concepts. I hope my determination comes through in this app, because I haven\'t worked with Flutter before.'
+      '\n\nI hope that DTT can look past this and give me a chance to become a top Flutter developer and thus further strengthen the DTT team.'
+      '\n\nPlease also take a look at my Github for my personal projects such as my smartwatch app and LinkedIn for more information about my background.';
+
+  static const mapsText = 'Location of house';
+
+  static const mapsLocationId = 'targetMarker';
+
+  static const emptyListWaring = 'No results found! \nPerhaps try another search?';
+}
